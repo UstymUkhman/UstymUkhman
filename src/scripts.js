5f94a9acd62d45b29496e18db1cbbbdc379f99e3
Ustym_Ukhman <ustym.ukhman@gmail.com>
2017-07-28 18:15:59 +0200

    Mobile responsive updates

--- src/scripts.js
+++ src/scripts.js
@@ -8,5 +8,12 @@ import 'babel-polyfill';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule              } from './components/app/app.module';
 
-window.isMobile = navigator.userAgent.toLocaleLowerCase().includes('mobile');
+window.isMobile = !!navigator.userAgent.match(/iPad/i)
+               || !!navigator.userAgent.match(/iPod/i)
+               || !!navigator.userAgent.match(/webOS/i)
+               || !!navigator.userAgent.match(/iPhone/i)
+               || !!navigator.userAgent.match(/Android/i)
+               || !!navigator.userAgent.match(/BlackBerry/i)
+               || !!navigator.userAgent.match(/Windows Phone/i);
+
 platformBrowserDynamic().bootstrapModule(AppModule);
