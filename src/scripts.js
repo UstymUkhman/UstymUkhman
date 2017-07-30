a9a442140791ea0c28421a45efdd87d44387c521
UstymUkhman <ustym.ukhman@gmail.com>
2017-07-30 12:27:38 +0200

    Build Failed

--- src/scripts.js
+++ src/scripts.js
@@ -3,10 +3,13 @@ const isIE = navigator.userAgent.indexOf('MSIE ')    > -1 ||
 
 if (isIE) document.getElementById('msie').style.display = 'block';
 
+
 import 'babel-polyfill';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule              } from './components/app/app.module';
+import { enableProdMode         } from '@angular/core';
+
 
 window.isMobile = !!navigator.userAgent.match(/iPad/i)
                || !!navigator.userAgent.match(/iPod/i)
@@ -16,4 +19,7 @@ window.isMobile = !!navigator.userAgent.match(/iPad/i)
                || !!navigator.userAgent.match(/BlackBerry/i)
                || !!navigator.userAgent.match(/Windows Phone/i);
 
-if (!isIE) platformBrowserDynamic().bootstrapModule(AppModule);
+if (!isIE) {
+  enableProdMode();
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}
