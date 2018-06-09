926bf84e769d2590afbd4fbcc63a5baa11fabbd6
UstymUkhman <ustym.ukhman@gmail.com>
2018-06-09 12:46:19 +0200

    Dependencies modules upgraded

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,9 +1,11 @@
 'use strict'
+
 const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
+
 const { VueLoaderPlugin } = require('vue-loader')
+const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -22,40 +24,58 @@ const createLintingRule = () => ({
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
+
   entry: {
     app: './src/main.js'
   },
+
   plugins: [
     new VueLoaderPlugin()
   ],
+
   output: {
-    path: config.build.assetsRoot,
     filename: '[name].js',
+    path: config.build.assetsRoot,
+
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+
   resolve: {
     extensions: ['.js', '.vue', '.json'],
+
     alias: {
       '@three': 'three-full/sources',
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
+
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
+
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
+
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('node_modules/webpack-dev-server/client'), resolve('node_modules/element-ui/src'), resolve('node_modules/element-ui/packages')]
+        include: [
+          resolve('src'),
+          resolve('node_modules/webpack-dev-server/client'),
+          resolve('node_modules/element-ui/src'),
+          resolve('node_modules/element-ui/packages'),
+          resolve('node_modules/vue-color/src'),
+          resolve('node_modules/vue-spinner/src'),
+          resolve('node_modules/three-full/sources')
+        ]
       },
+
       {
         test: /\.(png|jpe?g|gif)(\?.*)?$/,
         exclude: /3d(\\|\/)assets/,
@@ -65,6 +85,7 @@ module.exports = {
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
+
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
@@ -73,6 +94,7 @@ module.exports = {
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
+
       {
         test: /\.svg$/,
         loader: 'vue-svg-loader',
@@ -84,6 +106,7 @@ module.exports = {
           }
         }
       },
+
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
@@ -92,16 +115,19 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
+
       {
         test: /\.(glsl|vert|frag)$/,
         loader: 'threejs-glsl-loader'
       },
+
       {
         test: /\.(gltf)$/,
         loader: 'gltf-loader-2'
       },
+
       {
-        test: /3d(\\|\/)assets.*\.(bin|png|jpe?g|gif)$/,
+        test: /3d(\\|\/)assets.*\.(bin|png|jpe?g|gif|glb|json)$/,
         use: [
           {
             loader: 'file-loader',
@@ -113,6 +139,7 @@ module.exports = {
       }
     ]
   },
+
   node: {
     child_process: 'empty',
     setImmediate: false,
