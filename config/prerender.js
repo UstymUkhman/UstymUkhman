cb9b90eff44d55b11cdcbe2900355fe1fa8b4fb9
UstymUkhman <ustym.ukhman@gmail.com>
2020-03-22 10:48:34 +0200

    devDependencies & deploy.sh update

--- config/prerender.js
+++ config/prerender.js
@@ -1,17 +1,17 @@
-const experiments = require('../src/assets/data/experiments.json')
+// const experiments = require('../src/assets/data/experiments.json')
 
-const routes = [
-  '/',
-  '/about',
-  '/works',
-  '/contacts',
-  '/more',
-  '/hole',
-  '/experiments'
-]
+// const routes = [
+//   '/',
+//   '/about',
+//   '/works',
+//   '/contacts',
+//   '/more',
+//   '/hole',
+//   '/experiments'
+// ]
 
-for (let i = 0; i < experiments.length; i++) {
-  routes.push(`/experiments/${experiments[i].route}`)
-}
+// for (let i = 0; i < experiments.length; i++) {
+//   routes.push(`/experiments/${experiments[i].route}`)
+// }
 
-module.exports = routes
+// module.exports = routes
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ./{browserconfig.xml,sitemap.xml,manifest.json,robots.txt} ./dist/
+cp ./static/{browserconfig.xml,sitemap.xml,manifest.json,robots.txt} ./dist/
 sftp -i ./bitnami.pem bitnami@54.93.165.244:htdocs/ <<EOF
 put -r dist/*
 
