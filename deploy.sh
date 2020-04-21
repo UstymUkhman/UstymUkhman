4513863d159a2772bdc38573aa66e56cef99f9fb
Ustym <ustym.ukhman@gmail.com>
2020-04-21 09:00:37 +0300

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-domain="http://54.93.165.244/"
-date="$(date +'%Y-%m-%dT%H:%M:%S+00:00')"
-
 read -d '' sitemap << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
-    <loc>$domain</loc>
-    <lastmod>$date</lastmod>
+    <loc>$1</loc>
+    <lastmod>$(date +'%Y-%m-%dT%H:%M:%S+00:00')</lastmod>
   </url>
 </urlset>
 EOF
@@ -21,4 +18,3 @@ put -r dist/*
 
 exit
 EOF
-echo
