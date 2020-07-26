62dcef1edabf10317c45292f7c2f7e3fe2532885
Ustym <ustym.ukhman@gmail.com>
2020-07-26 15:12:54 +0300

    Deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 read -d '' sitemap << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
@@ -19,9 +21,30 @@ EOF
 echo "$robots" > ./public/robots.txt
 echo "$sitemap" > ./public/sitemap.xml
 
-cp ./public/{browserconfig.xml,sitemap.xml,manifest.json,robots.txt} ./dist/
-sftp -i ./bitnami.pem bitnami@$1:htdocs/ << EOF
-put -r dist/*
+echo "This script is going to deploy on production."$'\n'
+options=("Let's do this!" "Fuck, no!")
+
+PS3=$'\n'"Are your sure? "
+deploy=false
+
+select option in "${options[@]}"
+do
+  case $option in
+		"Let's do this!")
+			deploy=true
+      break ;;
+		*) break ;;
+  esac
+done
+
+clear
+
+if $deploy ; then
+
+  cp ./public/{browserconfig.xml,sitemap.xml,manifest.json,robots.txt} ./dist/
+  sftp -i ./bitnami.pem bitnami@$1:htdocs/ << EOF
+  put -r ./dist/*
 
 exit
 EOF
+fi
